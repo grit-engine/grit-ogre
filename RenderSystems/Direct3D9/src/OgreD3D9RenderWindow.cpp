c158f3c389aee1933e8897285e08ae7e2c6e39ac
Dave Cunningham <dcunnin@google.com>
2016-06-05 19:34:44 -0400

    Fix fullscreen in D3D9

--- RenderSystems/Direct3D9/src/OgreD3D9RenderWindow.cpp
+++ RenderSystems/Direct3D9/src/OgreD3D9RenderWindow.cpp
@@ -405,6 +405,7 @@ namespace Ogre
 				{
 					// was previously fullscreen, just changing the resolution
 					SetWindowPos(mHWnd, HWND_TOPMOST, mLeft, mTop, width, height, SWP_NOACTIVATE);
+                        mSwitchingFullscreen = true;
 				}
 				else
 				{
@@ -488,6 +489,12 @@ namespace Ogre
 
 			ULONG screenw = monitorInfo.rcWork.right  - monitorInfo.rcWork.left;
 			ULONG screenh = monitorInfo.rcWork.bottom - monitorInfo.rcWork.top;
+
+            // When switching from a low fullscreen res to windowed mode in a high
+            // res desktop need to release this low res constraint on the window
+            // region otherwise we will not be able to make the window larger
+            // than the low fullscreen res we were just at
+            SetWindowRgn(mHWnd, NULL, TRUE);
 
 			int left = screenw > winWidth ? ((screenw - winWidth) / 2) : 0;
 			int top = screenh > winHeight ? ((screenh - winHeight) / 2) : 0;
